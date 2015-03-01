4e31c590f6f65387a39d078573428b05c6ad30ce
juraj <jborza@gmail.com>
2015-03-01 21:16:31 +0100

    Removed dead About and Contact pages

--- ScrumPoker/Controllers/HomeController.cs
+++ ScrumPoker/Controllers/HomeController.cs
@@ -10,20 +10,6 @@ namespace ScrumPoker.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         public ActionResult Poker(int? roomID)
         {
             if (roomID == null)
